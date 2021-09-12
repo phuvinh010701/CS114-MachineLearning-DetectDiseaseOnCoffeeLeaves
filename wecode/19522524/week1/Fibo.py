e9d11d1989afbb71152ca40ababd6711d3cb3529
UIT.19522524 <58134921+phuvinh010701@users.noreply.github.com>
2021-09-12 14:52:55 +0700

    Assigment wecode

--- wecode/19522524/week1/Fibo.py
+++ wecode/19522524/week1/Fibo.py
@@ -1,14 +1,11 @@
-def fibo(n):
-    a = [1] * n
-
-    for i in range(2, n):
-        a[i] = a[i - 1] + a[i - 2]
-    return a[n - 1]
-
-def solve(n):
+def solve():
+    n = int(input())
     if 1 <= n <= 30:
-        print(fibo(n))
+        f = [1] * 31
+        for i in range(2, 31):
+            f[i] = f[i - 1] + f[i - 2]
+        print(f[n - 1])
     else:
         print("So {} khong nam trong khoang [1,30].".format(n))
 
-solve(int(input()))
+solve()
